6c1a2cd29d38a7a1531dd3acbaecb85912848566
xinghun <xinghun@alibaba-inc.com>
2018-03-25 20:49:18 +0800

    simple

--- 1/server.go
+++ 1/server.go
@@ -1,1 +1,53 @@
-package _
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"log"
+)
+
+func main() {
+	netListen, err := net.Listen("tcp", "localhost:1024")
+	CheckError(err)
+	defer netListen.Close()
+
+	Log("Waiting for clients")
+
+	for {
+		conn, err := netListen.Accept()
+		if err != nil {
+			continue
+		}
+
+		Log(conn.RemoteAddr().String(), " tcp connect success")
+		handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte,2048)
+
+	for {
+		n, err := conn.Read(buffer)
+
+		if err != nil{
+			Log(conn.RemoteAddr().String(), " connection error: ", err.Error())
+			return
+		}
+
+		Log(conn.RemoteAddr().String(), "receive data string:\n", string(buffer[:n]))
+	}
+}
+
+func Log(v ...interface{}) {
+	log.Println(v...)
+}
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
